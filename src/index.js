57b37f53fcf786e907b0f3d58dcdb81bd849b998
gudvinga <gudvinga@gmail.com>
2018-02-23 00:30:19 +0300

    WIP

--- src/index.js
+++ src/index.js
@@ -1,36 +1,60 @@
 class SmartCalculator {
   constructor(initialValue) {
     this.initialValue = initialValue;
-    this.res = initialValue;
-  }
-
-  toString () {
-    return this.res;
+    this.value = '' + this.initialValue;
+    this.prev = this.initialValue;
+    this.res = null;
+    this.expArr = [];
   }
 
   add(number) {
-    this.res += number;
+    this.setProperties(number);
+    this.value += `+${number}`;
     return this;
   }
-  
+
   subtract(number) {
-    this.res += number;
+    this.setProperties(number);
+    this.value += `-${number}`;
     return this;
   }
 
   multiply(number) {
-    this.res *= number;
+    this.setProperties(number);
+    this.value += `*${number}`;
     return this;
   }
 
   devide(number) {
-    this.res /= number;
+    this.setProperties(number);
+    this.value += `/${number}`;
     return this;
   }
 
   pow(number) {
-    this.res = Math.pow(this.res, number);
+    this.expArr.push(number);
+    const exp = this.expArr.reduceRight((a, b) => Math.pow(b, a), 1);
+
+    const length = this.res ? String(this.res).length : String(this.prev).length;
+
+    this.res = Math.pow(this.prev, exp);
+    this.value = this.value.slice(0, this.value.length - length) + this.res;
+
     return this;
+  }
+
+  setProperties(number) {
+    this.prev = number;
+    this.expArr = []
+    this.res = null;
+  }
+
+  valueOf() {
+    return eval(this.value);
+  }
+
+  toString() {
+    return eval(this.value);
   }
 }
 
