66d19f84e3a6dbdf2b6622b307841311b822db30
gudvinga <gudvinga@gmail.com>
2018-02-23 11:40:08 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,26 +1,39 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this.res = String(initialValue);
   }
 
   add(number) {
-    // your implementation
+    this.res += `+${number}`;
+    return this;
   }
   
   subtract(number) {
-    // your implementation
+    this.res += `-${number}`;
+    return this;
   }
 
   multiply(number) {
-    // your implementation
+    this.res += `*${number}`;
+    return this;
   }
 
   devide(number) {
-    // your implementation
+    this.res += `/${number}`;
+    return this;
   }
 
   pow(number) {
-    // your implementation
+    this.res += `**${number}`;
+    return this;
+  }
+
+  valueOf() {
+    return eval(this.res)
+  }
+
+  toString() {
+    return eval(this.res)
   }
 }
 
